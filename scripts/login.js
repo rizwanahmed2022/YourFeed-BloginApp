0db541e14851fe534853f061eae85f680d631d84
rizwanahmed2002 <irizwanms@gmail.com>
2025-11-06 01:17:34 +0500

    New App

---
+++ scripts/login.js
@@ -0,0 +1,25 @@
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { auth } from "./firebaseConfig.js";
+
+const form = document.querySelector("#form");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  console.log(email.value);
+  console.log(password.value);
+
+  signInWithEmailAndPassword(auth, email.value, password.value)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      window.location = "index.html"
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      alert("User Did't Exist")
+      
+    });
+});
