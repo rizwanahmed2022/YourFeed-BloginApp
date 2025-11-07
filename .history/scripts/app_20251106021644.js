36b6b8f06d76aceb4daeeba9949c6f3afa8f15ae
rizwanahmed2002 <irizwanms@gmail.com>
2025-11-07 22:19:25 +0500

    Update

---
+++ .history/scripts/app_20251106021644.js
@@ -0,0 +1,92 @@
+import {
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { auth } from "./firebaseConfig.js";
+import { db } from "./firebaseConfig.js";
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  where,
+} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+
+const logoutBtn = document.querySelector("#logout-btn");
+const signinBtn = document.querySelector("#signin-btn");
+const profilePic = document.querySelector(".profile-pic");
+const userName = document.querySelector(".user-name");
+const startWritin = document.querySelector(".startwritin");
+const navItem = document.querySelector(".nav-item");
+let userId = "";
+
+onAuthStateChanged(auth, (user) => {
+  const profilePic = document.querySelector(".profile-pic");
+
+  if (user) {
+    userId = user.uid;
+    console.log(user.uid);
+    signinBtn.style.display = "none";
+    logoutBtn.style.display = "block";
+    profilePic.style.display = "block";
+    userName.style.display = "block";
+    startWritin.style.display = "block";
+    navItem.style.display = "block";
+  } else {
+    signinBtn.style.display = "block";
+    logoutBtn.style.display = "none";
+    profilePic.style.display = "none";
+    userName.style.display = "none";
+    startWritin.style.display = "none";
+    navItem.style.display = "none";
+    console.log("not logged in");
+  }
+});
+
+logoutBtn.addEventListener("click", () => {
+  signOut(auth)
+    .then(() => {
+      window.location = "index.html";
+    })
+    .catch((error) => {
+      alert("error occured");
+    });
+});
+
+signinBtn.addEventListener("click", () => {
+  window.location = "login.html";
+});
+
+async function getDataFromDB(userId) {
+  const q = query(
+    collection(db, "users"),
+    where("uid", "==", uid)
+   
+  );
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((user)=>{
+    console.log(userInfo.fullname);
+    console.log(userInfo.profile);
+
+  }) // data() is synchronous
+  // profilePic.src = userInfo.profile;
+  // userName.innerHTML = userInfo.fullname;
+
+
+  return userInfo;
+}
+
+getDataFromDB(userId);
+
+
+
+sync function getDataFromDB(uid) {
+
+  
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    allTodo.push({ ...doc.data(), docid: doc.id });
+  });
+  console.log(allTodo);
+  renderTodo(allTodo);
+}
---
+++ .history/scripts/app_20251107213740.js
@@ -0,0 +1,81 @@
+import {
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { auth } from "./firebaseConfig.js";
+import { db } from "./firebaseConfig.js";
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  where,
+} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+
+const logoutBtn = document.querySelector("#logout-btn");
+const signinBtn = document.querySelector("#signin-btn");
+const profilePic = document.querySelector(".profile-pic");
+const userName = document.querySelector(".user-name");
+const startWritin = document.querySelector(".startwritin");
+const navItem = document.querySelector(".nav-item");
+let userId;
+
+onAuthStateChanged(auth, async (user) => {
+  const profilePic = document.querySelector(".profile-pic");
+
+  userId = await user.uid;
+  console.log("this is inside", userId);
+  console.log(user.userId;
+  if (user) {
+    signinBtn.style.display = "none";
+    logoutBtn.style.display = "block";
+    profilePic.style.display = "block";
+    userName.style.display = "block";
+    startWritin.style.display = "block";
+    navItem.style.display = "block";
+  } else {
+    signinBtn.style.display = "block";
+    logoutBtn.style.display = "none";
+    profilePic.style.display = "none";
+    userName.style.display = "none";
+    startWritin.style.display = "none";
+    navItem.style.display = "none";
+    console.log("not logged in");
+  }
+});
+
+logoutBtn.addEventListener("click", () => {
+  signOut(auth)
+    .then(() => {
+      window.location = "index.html";
+    })
+    .catch((error) => {
+      alert("error occured");
+    });
+});
+
+
+//goto login page
+signinBtn.addEventListener("click", () => {
+  window.location = "login.html";
+});
+
+
+
+
+async function getDataFromDB(userId) {
+  const q = query(
+    collection(db, "users")
+    // where("uid", "==", userId) // uid must be a string
+  );
+  const querySnapshot = await getDocs(q);
+  const userInfo = querySnapshot.docs[2].data(); // data() is synchronous
+  profilePic.src = userInfo.profile;
+  userName.innerHTML = userInfo.fullname;
+
+  // console.log(userInfo.fullname);
+  // console.log(userInfo.profile);
+
+  return userInfo;
+}
+getDataFromDB(userId);
